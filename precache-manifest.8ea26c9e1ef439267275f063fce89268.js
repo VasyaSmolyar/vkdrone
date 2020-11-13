44563f0f541295d6fb7ba2fb221595e4df28596f
Vasily Smolyar <smolyar4@gmail.com>
2020-11-13 21:44:34 +0300

    Updates

---
+++ precache-manifest.8ea26c9e1ef439267275f063fce89268.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ce5c60fc99d1567b89ddc4d815df6d",
+    "url": "/vkdrone/index.html"
+  },
+  {
+    "revision": "fc32c91d5b01d3ae3446",
+    "url": "/vkdrone/static/css/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "78b202b221b8c1c99709",
+    "url": "/vkdrone/static/css/main.b7cae34e.chunk.css"
+  },
+  {
+    "revision": "fc32c91d5b01d3ae3446",
+    "url": "/vkdrone/static/js/2.d39f3b98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/vkdrone/static/js/2.d39f3b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b202b221b8c1c99709",
+    "url": "/vkdrone/static/js/main.14de1a4b.chunk.js"
+  },
+  {
+    "revision": "09ff20ddbec9aacd1d4b",
+    "url": "/vkdrone/static/js/runtime-main.cb436749.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkdrone/static/media/persik.4e1ec840.png"
+  }
+]);
